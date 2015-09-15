ce7db75f3018979912b81a2bad71b9e4d43494e8
Yixuan Qiu <yixuan.qiu@cos.name>
2015-09-15 11:14:15 -0400

    documentation update

--- doc/classSpectra_1_1SymEigsSolver.js
+++ doc/classSpectra_1_1SymEigsSolver.js
@@ -3,7 +3,7 @@ var classSpectra_1_1SymEigsSolver =
     [ "SymEigsSolver", "classSpectra_1_1SymEigsSolver.html#aa43d8837ab70c5a102d4366ff3b0c490", null ],
     [ "init", "classSpectra_1_1SymEigsSolver.html#aaf2ef756971d56fb6a432efdf9c9a059", null ],
     [ "init", "classSpectra_1_1SymEigsSolver.html#a5720333d68d6146388f1e6ef8233ffd5", null ],
-    [ "compute", "classSpectra_1_1SymEigsSolver.html#a91f19eac2c739256eaae0cc45653d710", null ],
+    [ "compute", "classSpectra_1_1SymEigsSolver.html#acd644fd8368d2f944774fe32200f44f5", null ],
     [ "num_iterations", "classSpectra_1_1SymEigsSolver.html#a81df0bff7918c929f5d5bc7b6c35f27d", null ],
     [ "num_operations", "classSpectra_1_1SymEigsSolver.html#a08594fdd5e1d5d09cd54be3b06e07f03", null ],
     [ "eigenvalues", "classSpectra_1_1SymEigsSolver.html#a2d6bc02156838467b83ae5bfeaf92a53", null ],
