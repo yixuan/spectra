362abf93437b9b6ac88cc0dc92c407c7a7eca939
Yixuan Qiu <yixuan.qiu@cos.name>
2016-04-03 15:30:59 -0400

    test sparse matrix for GenEigsRealShift

--- test/GenEigsRealShift.cpp
+++ test/GenEigsRealShift.cpp
@@ -1,8 +1,11 @@
 #include <Eigen/Core>
+#include <Eigen/SparseCore>
 #include <iostream>
+#include <random> // Requires C++ 11
 
 #include <GenEigsSolver.h>
 #include <MatOp/DenseGenRealShiftSolve.h>
+#include <MatOp/SparseGenRealShiftSolve.h>
 
 using namespace Spectra;
 
@@ -13,20 +16,54 @@ typedef Eigen::MatrixXd Matrix;
 typedef Eigen::VectorXd Vector;
 typedef Eigen::MatrixXcd ComplexMatrix;
 typedef Eigen::VectorXcd ComplexVector;
+typedef Eigen::SparseMatrix<double> SpMatrix;
 
-template <int SelectionRule>
-void run_test(const Matrix &mat, int k, int m, double sigma)
+// Traits to obtain operation type from matrix type
+template <typename MatType>
+struct OpTypeTrait
 {
-    DenseGenRealShiftSolve<double> op(mat);
-    GenEigsRealShiftSolver<double, SelectionRule, DenseGenRealShiftSolve<double>> eigs(&op, k, m, sigma);
+    typedef DenseGenRealShiftSolve<double> OpType;
+};
+
+template <>
+struct OpTypeTrait<SpMatrix>
+{
+    typedef SparseGenRealShiftSolve<double> OpType;
+};
+
+// Generate random sparse matrix
+SpMatrix sprand(int size, double prob = 0.5)
+{
+    SpMatrix mat(size, size);
+    std::default_random_engine gen;
+    gen.seed(0);
+    std::uniform_real_distribution<double> distr(-1.0, 1.0);
+    for(int i = 0; i < size; i++)
+    {
+        for(int j = 0; j < size; j++)
+        {
+            if(distr(gen) < prob)
+                mat.insert(i, j) = distr(gen);
+        }
+    }
+    return mat;
+}
+
+
+template <typename MatType, int SelectionRule>
+void run_test(const MatType& mat, int k, int m, double sigma)
+{
+    typename OpTypeTrait<MatType>::OpType op(mat);
+    GenEigsRealShiftSolver<double, SelectionRule, typename OpTypeTrait<MatType>::OpType>
+        eigs(&op, k, m, sigma);
     eigs.init();
     int nconv = eigs.compute();
     int niter = eigs.num_iterations();
-    int nops = eigs.num_operations();
+    int nops  = eigs.num_operations();
 
     INFO( "nconv = " << nconv );
     INFO( "niter = " << niter );
-    INFO( "nops = " << nops );
+    INFO( "nops  = " << nops );
     REQUIRE( eigs.info() == SUCCESSFUL );
 
     ComplexVector evals = eigs.eigenvalues();
@@ -37,68 +74,103 @@ void run_test(const Matrix &mat, int k, int m, double sigma)
     REQUIRE( err.array().abs().maxCoeff() == Approx(0.0) );
 }
 
-
-void run_test_sets(const Matrix &A, int k, int m, double sigma)
+template <typename MatType>
+void run_test_sets(const MatType& A, int k, int m, double sigma)
 {
     SECTION( "Largest Magnitude" )
     {
-        run_test<LARGEST_MAGN>(A, k, m, sigma);
+        run_test<MatType, LARGEST_MAGN>(A, k, m, sigma);
     }
     SECTION( "Largest Real Part" )
     {
-        run_test<LARGEST_REAL>(A, k, m, sigma);
+        run_test<MatType, LARGEST_REAL>(A, k, m, sigma);
     }
     SECTION( "Largest Imaginary Part" )
     {
-        run_test<LARGEST_IMAG>(A, k, m, sigma);
+        run_test<MatType, LARGEST_IMAG>(A, k, m, sigma);
     }
     SECTION( "Smallest Magnitude" )
     {
-        run_test<SMALLEST_MAGN>(A, k, m, sigma);
+        run_test<MatType, SMALLEST_MAGN>(A, k, m, sigma);
     }
     SECTION( "Smallest Real Part" )
     {
-        run_test<SMALLEST_REAL>(A, k, m, sigma);
+        run_test<MatType, SMALLEST_REAL>(A, k, m, sigma);
     }
     SECTION( "Smallest Imaginary Part" )
     {
-        run_test<SMALLEST_IMAG>(A, k, m, sigma);
+        run_test<MatType, SMALLEST_IMAG>(A, k, m, sigma);
     }
 }
 
-
 TEST_CASE("Eigensolver of general real matrix [10x10]", "[eigs_gen]")
 {
-    srand(123);
+    std::srand(123);
 
-    Matrix A = Eigen::MatrixXd::Random(10, 10);
+    const Matrix A = Eigen::MatrixXd::Random(10, 10);
     int k = 3;
     int m = 6;
-    double sigma = 0.0;
+    double sigma = 1.0;
 
     run_test_sets(A, k, m, sigma);
 }
 
 TEST_CASE("Eigensolver of general real matrix [100x100]", "[eigs_gen]")
 {
-    srand(123);
+    std::srand(123);
 
-    Matrix A = Eigen::MatrixXd::Random(100, 100);
+    const Matrix A = Eigen::MatrixXd::Random(100, 100);
     int k = 10;
     int m = 20;
-    double sigma = 0.0;
+    double sigma = 10.0;
 
     run_test_sets(A, k, m, sigma);
 }
 
 TEST_CASE("Eigensolver of general real matrix [1000x1000]", "[eigs_gen]")
 {
-    srand(123);
+    std::srand(123);
 
-    Matrix A = Eigen::MatrixXd::Random(1000, 1000);
+    const Matrix A = Eigen::MatrixXd::Random(1000, 1000);
     int k = 20;
     int m = 50;
-    double sigma = 0.0;
+    double sigma = 100.0;
+
+    run_test_sets(A, k, m, sigma);
+}
+
+TEST_CASE("Eigensolver of sparse real matrix [10x10]", "[eigs_gen]")
+{
+    std::srand(123);
+
+    const SpMatrix A = sprand(10, 0.5);
+    int k = 3;
+    int m = 6;
+    double sigma = 1.0;
+
+    run_test_sets(A, k, m, sigma);
+}
+
+TEST_CASE("Eigensolver of sparse real matrix [100x100]", "[eigs_gen]")
+{
+    std::srand(123);
+
+    const SpMatrix A = sprand(100, 0.5);
+    int k = 10;
+    int m = 20;
+    double sigma = 10.0;
+
+    run_test_sets(A, k, m, sigma);
+}
+
+TEST_CASE("Eigensolver of sparse real matrix [1000x1000]", "[eigs_gen]")
+{
+    std::srand(123);
+
+    const SpMatrix A = sprand(1000, 0.5);
+    int k = 20;
+    int m = 50;
+    double sigma = 100.0;
 
     run_test_sets(A, k, m, sigma);
 }
