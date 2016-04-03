fdfe2fd7663311b7671d9ef40f69f65f09d70356
Yixuan Qiu <yixuan.qiu@cos.name>
2016-04-03 11:18:12 -0400

    add symmetric sparse matrix operation

---
+++ include/MatOp/SparseSymMatProd.h
@@ -0,0 +1,71 @@
+// Copyright (C) 2016 Yixuan Qiu <yixuan.qiu@cos.name>
+//
+// This Source Code Form is subject to the terms of the Mozilla
+// Public License v. 2.0. If a copy of the MPL was not distributed
+// with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+#ifndef SPARSE_SYM_MAT_PROD_H
+#define SPARSE_SYM_MAT_PROD_H
+
+#include <Eigen/Core>
+#include <Eigen/SparseCore>
+
+namespace Spectra {
+
+
+///
+/// \ingroup MatOp
+///
+/// This class defines the matrix-vector multiplication operation on a
+/// sparse symmetric real matrix \f$A\f$, i.e., calculating \f$y=Ax\f$ for any vector
+/// \f$x\f$. It is mainly used in the SymEigsSolver eigen solver.
+///
+template <typename Scalar, int Uplo = Eigen::Lower>
+class SparseSymMatProd
+{
+private:
+    typedef Eigen::Matrix<Scalar, Eigen::Dynamic, 1> Vector;
+    typedef Eigen::Map<Vector> MapVec;
+    typedef Eigen::SparseMatrix<Scalar> SparseMatrix;
+
+    const SparseMatrix& m_mat;
+
+public:
+    ///
+    /// Constructor to create the matrix operation object.
+    ///
+    /// \param mat_ An **Eigen** sparse matrix object, whose type is
+    /// `Eigen::SparseMatrix<Scalar, ...>`.
+    ///
+    SparseSymMatProd(const SparseMatrix& mat_) :
+        m_mat(mat_)
+    {}
+
+    ///
+    /// Return the number of rows of the underlying matrix.
+    ///
+    int rows() const { return m_mat.rows(); }
+    ///
+    /// Return the number of columns of the underlying matrix.
+    ///
+    int cols() const { return m_mat.cols(); }
+
+    ///
+    /// Perform the matrix-vector multiplication operation \f$y=Ax\f$.
+    ///
+    /// \param x_in  Pointer to the \f$x\f$ vector.
+    /// \param y_out Pointer to the \f$y\f$ vector.
+    ///
+    // y_out = A * x_in
+    void perform_op(Scalar *x_in, Scalar *y_out) const
+    {
+        MapVec x(x_in, m_mat.cols());
+        MapVec y(y_out, m_mat.rows());
+        y.noalias() = m_mat.template selfadjointView<Uplo>() * x;
+    }
+};
+
+
+} // namespace Spectra
+
+#endif // SPARSE_SYM_MAT_PROD_H
