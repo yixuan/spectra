8630cec1d50d07e52fa45591739bbbf491cc324c
Yixuan Qiu <yixuan.qiu@cos.name>
2016-10-22 11:39:48 -0400

    sparse Cholesky

---
+++ include/MatOp/SparseCholesky.h
@@ -0,0 +1,95 @@
+// Copyright (C) 2016 Yixuan Qiu <yixuan.qiu@cos.name>
+//
+// This Source Code Form is subject to the terms of the Mozilla
+// Public License v. 2.0. If a copy of the MPL was not distributed
+// with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+#ifndef SPARSE_CHOLESKY_H
+#define SPARSE_CHOLESKY_H
+
+#include <Eigen/Core>
+#include <Eigen/Cholesky>
+#include <stdexcept>
+
+namespace Spectra {
+
+
+///
+/// \ingroup MatOp
+///
+/// This class defines the operations related to Cholesky decomposition on a
+/// sparse positive definite matrix, \f$A=LL'\f$, where \f$L\f$ is a lower triangular
+/// matrix. It is mainly used in the SymGEigsSolver generalized eigen solver
+/// in the Cholesky decomposition mode.
+///
+template <typename Scalar, int Uplo = Eigen::Lower>
+class SparseCholesky
+{
+private:
+    typedef Eigen::Matrix<Scalar, Eigen::Dynamic, 1> Vector;
+    typedef Eigen::Map<Vector> MapVec;
+    typedef Eigen::SparseMatrix<Scalar> SparseMatrix;
+
+    const int m_n;
+    Eigen::SimplicialLLT<SparseMatrix, Uplo> m_decomp;
+
+public:
+    ///
+    /// Constructor to create the matrix operation object.
+    ///
+    /// \param mat_ An **Eigen** sparse matrix object, whose type is
+    /// `Eigen::SparseMatrix<Scalar, ...>`.
+    ///
+    SparseCholesky(const SparseMatrix& mat_) :
+        m_n(mat_.rows())
+    {
+        if(mat_.rows() != mat_.cols())
+            throw std::invalid_argument("SparseCholesky: matrix must be square");
+
+        m_decomp.compute(mat_);
+    }
+
+    ///
+    /// Return the number of rows of the underlying matrix.
+    ///
+    int rows() const { return m_n; }
+    ///
+    /// Return the number of columns of the underlying matrix.
+    ///
+    int cols() const { return m_n; }
+
+    ///
+    /// Perform the lower triangular solving operation \f$y=L^{-1}x\f$.
+    ///
+    /// \param x_in  Pointer to the \f$x\f$ vector.
+    /// \param y_out Pointer to the \f$y\f$ vector.
+    ///
+    // y_out = inv(L) * x_in
+    void lower_triangular_solve(Scalar* x_in, Scalar* y_out) const
+    {
+        MapVec x(x_in,  m_n);
+        MapVec y(y_out, m_n);
+        y.noalias() = m_decomp.permutationP() * x;
+        m_decomp.matrixL().solveInPlace(y);
+    }
+
+    ///
+    /// Perform the upper triangular solving operation \f$y=(L')^{-1}x\f$.
+    ///
+    /// \param x_in  Pointer to the \f$x\f$ vector.
+    /// \param y_out Pointer to the \f$y\f$ vector.
+    ///
+    // y_out = inv(L') * x_in
+    void upper_triangular_solve(Scalar* x_in, Scalar* y_out) const
+    {
+        MapVec x(x_in,  m_n);
+        MapVec y(y_out, m_n);
+        y.noalias() = m_decomp.matrixU().solve(x);
+        y = m_decomp.permutationPinv() * y;
+    }
+};
+
+
+} // namespace Spectra
+
+#endif // SPARSE_CHOLESKY_H
