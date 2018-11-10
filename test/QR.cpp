32822ffe2a80bb248e52993c203a95c3ffccbd94
Yixuan Qiu <yixuanq@gmail.com>
2018-11-10 13:23:33 -0500

    test for shifts

--- test/QR.cpp
+++ test/QR.cpp
@@ -15,10 +15,12 @@ using Eigen::VectorXd;
 typedef Eigen::Map<Eigen::MatrixXd> MapMat;
 
 template <typename Solver, typename MatrixType>
-void run_test(MatrixType &H)
+void run_test(MatrixType &H, double shift)
 {
-    Solver decomp(H);
-    int n = H.rows();
+    Solver decomp(H, shift);
+    const int n = H.rows();
+    const double tol = 1e-12;
+    MatrixXd Hs = H - shift * MatrixXd::Identity(n, n);
 
     // Obtain Q matrix
     MatrixXd I = MatrixXd::Identity(n, n);
@@ -28,30 +30,28 @@ void run_test(MatrixType &H)
     // Test orthogonality
     MatrixXd QtQ = Q.transpose() * Q;
     INFO( "||Q'Q - I||_inf = " << (QtQ - I).cwiseAbs().maxCoeff() );
-    REQUIRE( (QtQ - I).cwiseAbs().maxCoeff() == Approx(0.0).margin(1e-12) );
+    REQUIRE( (QtQ - I).cwiseAbs().maxCoeff() == Approx(0.0).margin(tol) );
 
     MatrixXd QQt = Q * Q.transpose();
     INFO( "||QQ' - I||_inf = " << (QQt - I).cwiseAbs().maxCoeff() );
-    REQUIRE( (QQt - I).cwiseAbs().maxCoeff() == Approx(0.0).margin(1e-12) );
+    REQUIRE( (QQt - I).cwiseAbs().maxCoeff() == Approx(0.0).margin(tol) );
 
-    // Calculate R = Q'H
+    // Obtain R matrix and test whether it is upper triangular
     MatrixXd R = decomp.matrix_R();
-    MatrixXd Rlower = R.triangularView<Eigen::Lower>();
-    Rlower.diagonal().setZero();
+    MatrixXd Rlower = R.triangularView<Eigen::StrictlyLower>();
     INFO( "Whether R is upper triangular, error = " << Rlower.cwiseAbs().maxCoeff() );
-    REQUIRE( Rlower.cwiseAbs().maxCoeff() == Approx(0.0).margin(1e-12) );
+    REQUIRE( Rlower.cwiseAbs().maxCoeff() == Approx(0.0).margin(tol) );
 
-    // Compare H and QR
-    INFO( "||H - QR||_inf = " << (H - Q * R).cwiseAbs().maxCoeff() );
-    REQUIRE( (H - Q * R).cwiseAbs().maxCoeff() == Approx(0.0).margin(1e-12) );
+    // Compare Hs = H - s * I and QR
+    INFO( "||Hs - QR||_inf = " << (Hs - Q * R).cwiseAbs().maxCoeff() );
+    REQUIRE( (Hs - Q * R).cwiseAbs().maxCoeff() == Approx(0.0).margin(tol) );
 
-    // Test RQ
-    MatrixXd RQ_true = R;
-    decomp.apply_YQ(RQ_true);
-    MatrixXd RQ;
-    decomp.matrix_QtHQ(RQ);
-    INFO( "max error of RQ = " << (RQ - RQ_true).cwiseAbs().maxCoeff() );
-    REQUIRE( (RQ - RQ_true).cwiseAbs().maxCoeff() == Approx(0.0).margin(1e-12) );
+    // Obtain Q'HQ
+    MatrixXd QtHQ_true = Q.transpose() * H * Q;
+    MatrixXd QtHQ;
+    decomp.matrix_QtHQ(QtHQ);
+    INFO( "max error of Q'HQ = " << (QtHQ - QtHQ_true).cwiseAbs().maxCoeff() );
+    REQUIRE( (QtHQ - QtHQ_true).cwiseAbs().maxCoeff() == Approx(0.0).margin(tol) );
 
     // Test "apply" functions
     MatrixXd Y = MatrixXd::Random(n, n);
@@ -59,22 +59,22 @@ void run_test(MatrixType &H)
     MatrixXd QY = Y;
     decomp.apply_QY(QY);
     INFO( "max error of QY = " << (QY - Q * Y).cwiseAbs().maxCoeff() );
-    REQUIRE( (QY - Q * Y).cwiseAbs().maxCoeff() == Approx(0.0).margin(1e-12) );
+    REQUIRE( (QY - Q * Y).cwiseAbs().maxCoeff() == Approx(0.0).margin(tol) );
 
     MatrixXd YQ = Y;
     decomp.apply_YQ(YQ);
     INFO( "max error of YQ = " << (YQ - Y * Q).cwiseAbs().maxCoeff() );
-    REQUIRE( (YQ - Y * Q).cwiseAbs().maxCoeff() == Approx(0.0).margin(1e-12) );
+    REQUIRE( (YQ - Y * Q).cwiseAbs().maxCoeff() == Approx(0.0).margin(tol) );
 
     MatrixXd QtY = Y;
     decomp.apply_QtY(QtY);
     INFO( "max error of Q'Y = " << (QtY - Q.transpose() * Y).cwiseAbs().maxCoeff() );
-    REQUIRE( (QtY - Q.transpose() * Y).cwiseAbs().maxCoeff() == Approx(0.0).margin(1e-12) );
+    REQUIRE( (QtY - Q.transpose() * Y).cwiseAbs().maxCoeff() == Approx(0.0).margin(tol) );
 
     MatrixXd YQt = Y;
     decomp.apply_YQt(YQt);
     INFO( "max error of YQ' = " << (YQt - Y * Q.transpose()).cwiseAbs().maxCoeff() );
-    REQUIRE( (YQt - Y * Q.transpose()).cwiseAbs().maxCoeff() == Approx(0.0).margin(1e-12) );
+    REQUIRE( (YQt - Y * Q.transpose()).cwiseAbs().maxCoeff() == Approx(0.0).margin(tol) );
 
     // Test "apply" functions for vectors
     VectorXd y = VectorXd::Random(n);
@@ -82,12 +82,12 @@ void run_test(MatrixType &H)
     VectorXd Qy = y;
     decomp.apply_QY(Qy);
     INFO( "max error of Qy = " << (Qy - Q * y).cwiseAbs().maxCoeff() );
-    REQUIRE( (Qy - Q * y).cwiseAbs().maxCoeff() == Approx(0.0).margin(1e-12) );
+    REQUIRE( (Qy - Q * y).cwiseAbs().maxCoeff() == Approx(0.0).margin(tol) );
 
     VectorXd Qty = y;
     decomp.apply_QtY(Qty);
     INFO( "max error of Q'y = " << (Qty - Q.transpose() * y).cwiseAbs().maxCoeff() );
-    REQUIRE( (Qty - Q.transpose() * y).cwiseAbs().maxCoeff() == Approx(0.0).margin(1e-12) );
+    REQUIRE( (Qty - Q.transpose() * y).cwiseAbs().maxCoeff() == Approx(0.0).margin(tol) );
 }
 
 TEST_CASE("QR of upper Hessenberg matrix", "[QR]")
@@ -99,10 +99,10 @@ TEST_CASE("QR of upper Hessenberg matrix", "[QR]")
     MatrixXd H = m.triangularView<Eigen::Upper>();
     H.diagonal(-1) = m.diagonal(-1);
 
-    run_test< UpperHessenbergQR<double> >(H);
+    run_test< UpperHessenbergQR<double> >(H, 1.2345);
 
     MapMat Hmap(H.data(), H.rows(), H.cols());
-    run_test< UpperHessenbergQR<double> >(Hmap);
+    run_test< UpperHessenbergQR<double> >(Hmap, 0.6789);
 }
 
 TEST_CASE("QR of Tridiagonal matrix", "[QR]")
@@ -116,23 +116,25 @@ TEST_CASE("QR of Tridiagonal matrix", "[QR]")
     H.diagonal(-1) = m.diagonal(-1);
     H.diagonal(1) = m.diagonal(-1);
 
-    run_test< TridiagQR<double> >(H);
+    run_test< TridiagQR<double> >(H, 1.2345);
 
     MapMat Hmap(H.data(), H.rows(), H.cols());
-    run_test< TridiagQR<double> >(Hmap);
+    run_test< TridiagQR<double> >(Hmap, 0.6789);
 }
 
-TEST_CASE("QR decomposition with double shifts", "QR")
+TEST_CASE("QR decomposition with double shifts", "[QR]")
 {
     std::srand(123);
-    int n = 100;
+    const int n = 100;
+    const double tol = 1e-12;
+
     MatrixXd m = MatrixXd::Random(n, n);
     m.array() -= 0.5;
     MatrixXd H = m.triangularView<Eigen::Upper>();
     H.diagonal(-1) = m.diagonal(-1);
     H(1, 0) = 0;  // Test for the case when sub-diagonal element is zero
 
-    double s = 2, t = 3;
+    const double s = 2, t = 3;
 
     MatrixXd M = H * H - s * H + t * MatrixXd::Identity(n, n);
     Eigen::HouseholderQR<MatrixXd> qr(M);
@@ -144,13 +146,13 @@ TEST_CASE("QR decomposition with double shifts", "QR")
 
     // Equal up to signs
     INFO( "max error of Q = " << (Q.cwiseAbs() - Q0.cwiseAbs()).cwiseAbs().maxCoeff() );
-    REQUIRE( (Q.cwiseAbs() - Q0.cwiseAbs()).cwiseAbs().maxCoeff() == Approx(0.0).margin(1e-12) );
+    REQUIRE( (Q.cwiseAbs() - Q0.cwiseAbs()).cwiseAbs().maxCoeff() == Approx(0.0).margin(tol) );
 
     // Test Q'HQ
     MatrixXd QtHQ;
     decomp.matrix_QtHQ(QtHQ);
     INFO( "max error of Q'HQ = " << (QtHQ - Q.transpose() * H * Q).cwiseAbs().maxCoeff() );
-    REQUIRE( (QtHQ - Q.transpose() * H * Q).cwiseAbs().maxCoeff() == Approx(0.0).margin(1e-12) );
+    REQUIRE( (QtHQ - Q.transpose() * H * Q).cwiseAbs().maxCoeff() == Approx(0.0).margin(tol) );
 
     // Test apply functions
     VectorXd y = VectorXd::Random(n);
@@ -159,10 +161,10 @@ TEST_CASE("QR decomposition with double shifts", "QR")
     VectorXd Qty = y;
     decomp.apply_QtY(Qty);
     INFO( "max error of Q'y = " << (Qty - Q.transpose() * y).cwiseAbs().maxCoeff() );
-    REQUIRE( (Qty - Q.transpose() * y).cwiseAbs().maxCoeff() == Approx(0.0).margin(1e-12) );
+    REQUIRE( (Qty - Q.transpose() * y).cwiseAbs().maxCoeff() == Approx(0.0).margin(tol) );
 
     MatrixXd YQ = Y;
     decomp.apply_YQ(YQ);
     INFO( "max error of YQ = " << (YQ- Y * Q).cwiseAbs().maxCoeff() );
-    REQUIRE( (YQ- Y * Q).cwiseAbs().maxCoeff() == Approx(0.0).margin(1e-12) );
+    REQUIRE( (YQ- Y * Q).cwiseAbs().maxCoeff() == Approx(0.0).margin(tol) );
 }
