2264034f088d64c0e1a1ce8ff595714c725e84e5
Yixuan Qiu <yixuanq@gmail.com>
2020-06-04 14:27:59 -0400

    more accurate computation of Householder reflectors

--- include/Spectra/LinAlg/DoubleShiftQR.h
+++ include/Spectra/LinAlg/DoubleShiftQR.h
@@ -10,6 +10,7 @@
 #include <Eigen/Core>
 #include <vector>     // std::vector
 #include <algorithm>  // std::min, std::fill, std::copy
+#include <utility>    // std::swap
 #include <cmath>      // std::abs, std::sqrt, std::pow
 #include <stdexcept>  // std::invalid_argument, std::logic_error
 
@@ -35,7 +36,6 @@ private:
     static constexpr Scalar m_near_0 = TypeTraits<Scalar>::min() * Scalar(10);
     // The machine precision, ~= 1e-16 for the "double" type
     static constexpr Scalar m_eps = TypeTraits<Scalar>::epsilon();
-    static constexpr Scalar m_eps_rel = TypeTraits<Scalar>::epsilon();
 
     Index m_n;          // Dimension of the matrix
     Matrix m_mat_H;     // A copy of the matrix to be factorized
@@ -46,8 +46,58 @@ private:
                         // 3 - A general reflector
                         // 2 - A Givens rotation
                         // 1 - An identity transformation
-    const Scalar m_eps_abs;
-    bool m_computed;  // Whether matrix has been factorized
+    bool m_computed;    // Whether matrix has been factorized
+
+    // Compute sqrt(x1^2 + x2^2 + x3^2) wit high precision
+    static Scalar stable_norm3(Scalar x1, Scalar x2, Scalar x3)
+    {
+        using std::abs;
+        using std::sqrt;
+
+        x1 = abs(x1);
+        x2 = abs(x2);
+        x3 = abs(x3);
+        // Make x1 >= {x2, x3}
+        if (x1 < x2) std::swap(x1, x2);
+        if (x1 < x3) std::swap(x1, x3);
+        // If x1 is too small, return 0
+        if (x1 < m_near_0)
+            return Scalar(0);
+
+        const Scalar r2 = x2 / x1, r3 = x3 / x1;
+        // We choose a cutoff such that cutoff^4 < eps
+        // If max(r2, r3) > cutoff, use the standard way; otherwise use Taylor series expansion
+        // to avoid an explicit sqrt() call that may lose precision
+        const Scalar cutoff = Scalar(0.1) * pow(m_eps, Scalar(0.25));
+        Scalar r = r2 * r2 + r3 * r3;
+        r = (r2 >= cutoff || r3 >= cutoff) ?
+            sqrt(Scalar(1) + r) :
+            (Scalar(1) + r * (Scalar(0.5) - Scalar(0.125) * r));  // sqrt(1 + t) ~= 1 + t/2 - t^2/8
+        return x1 * r;
+    }
+
+    // x[i] <- x[i] / r, r = sqrt(x1^2 + x2^2 + x3^2)
+    // Assume |x1| >= {|x2|, |x3|}, x1 != 0
+    static void stable_scaling(Scalar& x1, Scalar& x2, Scalar& x3)
+    {
+        using std::abs;
+        using std::pow;
+        using std::sqrt;
+
+        const Scalar x1sign = (x1 > Scalar(0)) ? Scalar(1) : Scalar(-1);
+        x1 = abs(x1);
+        // Use the same method as in stable_norm3()
+        const Scalar r2 = x2 / x1, r3 = x3 / x1;
+        const Scalar cutoff = Scalar(0.1) * pow(m_eps, Scalar(0.25));
+        Scalar r = r2 * r2 + r3 * r3;
+        // r = 1/sqrt(1 + r2^2 + r3^2)
+        r = (abs(r2) >= cutoff || abs(r3) >= cutoff) ?
+            Scalar(1) / sqrt(Scalar(1) + r) :
+            (Scalar(1) - r * (Scalar(0.5) - Scalar(0.375) * r));  // 1/sqrt(1 + t) ~= 1 - t * (1/2 - (3/8) * t)
+        x1 = x1sign * r;
+        x2 = r2 * r;
+        x3 = r3 * r;
+    }
 
     void compute_reflector(const Scalar& x1, const Scalar& x2, const Scalar& x3, Index ind)
     {
@@ -55,42 +105,39 @@ private:
 
         Scalar* u = &m_ref_u.coeffRef(0, ind);
         unsigned char* nr = m_ref_nr.data();
-        // In general case the reflector affects 3 rows
-        nr[ind] = 3;
-        Scalar x2x3 = Scalar(0);
-        // If x3 is zero, decrease nr by 1
-        if (abs(x3) < m_near_0)
-        {
-            // If x2 is also zero, nr will be 1, and we can exit this function
-            if (abs(x2) < m_near_0)
-            {
-                nr[ind] = 1;
-                return;
-            }
-            else
-            {
-                nr[ind] = 2;
-            }
-            x2x3 = abs(x2);
-        }
-        else
-        {
-            x2x3 = Eigen::numext::hypot(x2, x3);
-        }
-
-        // x1' = x1 - rho * ||x||
-        // rho = -sign(x1), if x1 == 0, we choose rho = 1
-        Scalar x1_new = x1 - ((x1 <= 0) - (x1 > 0)) * Eigen::numext::hypot(x1, x2x3);
-        Scalar x_norm = Eigen::numext::hypot(x1_new, x2x3);
-        // Double check the norm of new x
-        if (x_norm < m_near_0)
+        const Scalar x2m = abs(x2), x3m = abs(x3);
+        // If both x2 and x3 are zero, nr is 1, and we early exit
+        if (x2m < m_near_0 && x3m < m_near_0)
         {
             nr[ind] = 1;
             return;
         }
-        u[0] = x1_new / x_norm;
-        u[1] = x2 / x_norm;
-        u[2] = x3 / x_norm;
+
+        // In general case the reflector affects 3 rows
+        // If x3 is zero, decrease nr by 1
+        nr[ind] = (x3m < m_near_0) ? 2 : 3;
+        const Scalar x_norm = (x3m < m_near_0) ? Eigen::numext::hypot(x1, x2) : stable_norm3(x1, x2, x3);
+
+        // x1' = x1 - rho * ||x||
+        // rho = -sign(x1), if x1 == 0, we choose rho = 1
+        const Scalar rho = (x1 <= Scalar(0)) - (x1 > Scalar(0));
+        const Scalar x1_new = x1 - rho * x_norm, x1m = abs(x1_new);
+        // Copy x to u
+        u[0] = x1_new;
+        u[1] = x2;
+        u[2] = x3;
+        if (x1m >= x2m && x1m >= x3m)
+        {
+            stable_scaling(u[0], u[1], u[2]);
+        }
+        else if (x2m >= x1m && x2m >= x3m)
+        {
+            stable_scaling(u[1], u[0], u[2]);
+        }
+        else
+        {
+            stable_scaling(u[2], u[0], u[1]);
+        }
     }
 
     void compute_reflector(const Scalar* x, Index ind)
@@ -170,10 +217,8 @@ private:
         if (nr == 1)
             return;
 
-        const Scalar u0 = m_ref_u.coeff(0, u_ind),
-                     u1 = m_ref_u.coeff(1, u_ind);
-        const Scalar u0_2 = Scalar(2) * u0,
-                     u1_2 = Scalar(2) * u1;
+        const Scalar u0 = m_ref_u.coeff(0, u_ind), u1 = m_ref_u.coeff(1, u_ind);
+        const Scalar u0_2 = Scalar(2) * u0, u1_2 = Scalar(2) * u1;
 
         const Index nrow = X.rows();
         const Index ncol = X.cols();
@@ -230,10 +275,8 @@ private:
         if (nr == 1)
             return;
 
-        const Scalar u0 = m_ref_u.coeff(0, u_ind),
-                     u1 = m_ref_u.coeff(1, u_ind);
-        const Scalar u0_2 = Scalar(2) * u0,
-                     u1_2 = Scalar(2) * u1;
+        const Scalar u0 = m_ref_u.coeff(0, u_ind), u1 = m_ref_u.coeff(1, u_ind);
+        const Scalar u0_2 = Scalar(2) * u0, u1_2 = Scalar(2) * u1;
 
         const int nrow = X.rows();
         const int ncol = X.cols();
@@ -269,7 +312,6 @@ private:
 public:
     DoubleShiftQR(Index size) :
         m_n(size),
-        m_eps_abs(m_near_0 * (m_n / m_eps)),
         m_computed(false)
     {}
 
@@ -280,7 +322,6 @@ public:
         m_shift_t(t),
         m_ref_u(3, m_n),
         m_ref_nr(m_n),
-        m_eps_abs(m_near_0 * (m_n / m_eps)),
         m_computed(false)
     {
         compute(mat, s, t);
@@ -301,19 +342,25 @@ public:
         m_ref_nr.resize(m_n);
 
         // Make a copy of mat
-        std::copy(mat.data(), mat.data() + mat.size(), m_mat_H.data());
+        m_mat_H.noalias() = mat;
 
         // Obtain the indices of zero elements in the subdiagonal,
         // so that H can be divided into several blocks
+        const Scalar eps_abs = m_near_0 * (m_n / m_eps);
+        constexpr Scalar eps_rel = m_eps;
         std::vector<int> zero_ind;
         zero_ind.reserve(m_n - 1);
         zero_ind.push_back(0);
         Scalar* Hii = m_mat_H.data();
         for (Index i = 0; i < m_n - 2; i++, Hii += (m_n + 1))
         {
+            // Hii[0] => m_mat_H(i, i)
             // Hii[1] => m_mat_H(i + 1, i)
+            // Hii[m_n + 1] => m_mat_H(i + 1, i + 1)
             const Scalar h = abs(Hii[1]);
-            if (h <= 0 || h <= m_eps_rel * (abs(Hii[0]) + abs(Hii[m_n + 1])))
+            // Deflate small sub-diagonal elements
+            const Scalar diag = abs(Hii[0]) + abs(Hii[m_n + 1]);
+            if (h <= eps_abs || h <= eps_rel * diag)
             {
                 Hii[1] = 0;
                 zero_ind.push_back(i + 1);
@@ -324,7 +371,8 @@ public:
         }
         zero_ind.push_back(m_n);
 
-        for (std::vector<int>::size_type i = 0; i < zero_ind.size() - 1; i++)
+        const Index len = zero_ind.size() - 1;
+        for (Index i = 0; i < len; i++)
         {
             const Index start = zero_ind[i];
             const Index end = zero_ind[i + 1] - 1;
