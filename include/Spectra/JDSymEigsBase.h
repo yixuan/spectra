94d44b427686f81ba0e3adf3d80b55de2f4455a1
jwehner <jenswehner@gmail.com>
2020-06-29 12:13:00 +0200

    Implemented first iteration of algorithm

--- include/Spectra/JDSymEigsBase.h
+++ include/Spectra/JDSymEigsBase.h
@@ -21,7 +21,6 @@
 #include "Util/SimpleRandom.h"
 namespace Spectra {
 
-
 template <typename Scalar,
           typename OpType>
 class JDSymEigsBase
@@ -41,7 +40,9 @@ public:
     JDSymEigsBase(OpType& op, Index nev) :
         m_op(op),
         m_n(op.rows()),
-        m_nev(nev)
+        m_nev(nev),
+        m_max_search_space_size(10*m_nev),
+        m_initial_search_space_size(2*m_nev)
     {
         check_argument();
     }
@@ -51,9 +52,26 @@ public:
         m_op_container(create_op_container(std::move(op))),
         m_op(m_op_container.front()),
         m_n(m_op.rows()),
-        m_nev(nev)
+        m_nev(nev),
+        m_max_search_space_size(10*m_nev),
+        m_initial_search_space_size(2*m_nev)
     {
         check_argument();
+    }
+
+    ///
+    /// Sets the Maxmium SearchspaceSize after which is deflated
+    ///
+    void setMaxSearchspaceSize(Index max_search_space_size){
+        m_max_search_space_size=max_search_space_size;
+    }
+
+
+    ///
+    /// Sets the Initial SearchspaceSize for Ritz values
+    ///
+    void setInitialSearchspaceSize(Index initial_search_space_size){
+        m_initial_search_space_size=initial_search_space_size;
     }
 
     ///
@@ -61,18 +79,89 @@ public:
     ///
     virtual ~JDSymEigsBase() {}
 
+    ///
+    /// Returns the status of the computation.
+    /// The full list of enumeration values can be found in \ref Enumerations.
+    ///
+    CompInfo info() const { return m_info; }
+
+    ///
+    /// Returns the number of iterations used in the computation.
+    ///
+    Index num_iterations() const { return m_niter; }
+
+    ///
+    /// Returns the number of matrix operations used in the computation.
+    ///
+    Index num_operations() const { return m_nmatop; }
+
+    struct RitzEigenPair
+    {
+        Vector lambda;  // eigenvalues
+        Matrix q;       // Ritz (or harmonic Ritz) eigenvectors
+        Matrix U;       // eigenvectors of the small subspace
+        Matrix res;     // residues of the pairs
+        Array res_norm() const
+        {
+            return res.colwise().norm();
+        }  // norm of the residues
+    };
+
+    struct ProjectedSpace
+    {
+        Matrix vect;   // basis of vectors
+        Matrix m_vect;  // A * V
+        Matrix vect_m_vect;   // V.T * A * V
+        Index current_size() const
+        {
+            return vect.cols();
+        };                                 // size of the projection i.e. number of cols in V
+        Index size_update;                 // size update ...
+        BoolArray root_converged;  // keep track of which root have onverged
+    };
+
+    ///
+    /// Initializes the solver by providing an initial search space.
+    ///
+    /// \param init_resid Matrix containing initial search space.
+    ///
+    ///
+    void init(const Matrix& init_space)
+    {
+        m_nmatop = 0;
+        m_niter = 0;
+    }
+
+    ///
+    /// Initializes the solver by providing  initial search sapce.
+    ///
+/// Depending n the method this search space is initialized in a different way
+    ///
+    void init()
+    {
+        Matrix intial_space = SetupInitialSearchSpace();
+        init(intial_space);
+    }
+
 protected:
+    virtual Matrix SetupInitialSearchSpace() const = 0;
+
+    virtual Matrix CalculateCorrectionVector() const = 0;
     std::vector<OpType> m_op_container;
     OpType& m_op;  // object to conduct marix operation,
                    // e.g. matrix-vector product
 
     Index m_niter = 0;
-    const Index m_n;     // dimension of matrix A
-    const Index m_nev;   // number of eigenvalues requested
-    Index m_nmatop = 0;  // number of matrix operations called
+    const Index m_n;             // dimension of matrix A
+    const Index m_nev;           // number of eigenvalues requested
+    Index m_max_search_space_size;
+    Index m_initial_search_space_size;
+    Index m_nmatop = 0;          // number of matrix operations called
+    RitzEigenPair m_ritz_pairs;  // Ritz eigen pair structure
+    ProjectedSpace m_proj_space; // Projected Space structure     
 
 private:
-    CompInfo m_info=CompInfo::NotComputed;  // status of the computation
+    CompInfo m_info = CompInfo::NotComputed;  // status of the computation
 
     // Move rvalue object to the container
     static std::vector<OpType> create_op_container(OpType&& rval)
@@ -88,38 +177,59 @@ private:
             throw std::invalid_argument("nev must satisfy 1 <= nev <= n - 1, n is the size of matrix");
     }
 
-    // struct RitzEigenPair {
-    //     Eigen::VectorXd lambda;  // eigenvalues
-    //     Eigen::MatrixXd q;       // Ritz (or harmonic Ritz) eigenvectors
-    //     Eigen::MatrixXd U;       // eigenvectors of the small subspace
-    //     Eigen::MatrixXd res;     // residues of the pairs
-    //     Eigen::ArrayXd res_norm() const {
-    //     return res.colwise().norm();
-    //     }  // norm of the residues
-    // };
+    void restart(Index size) {
+        m_proj_space.vect = m_ritz_pairs.q.leftCols(size);
+        m_proj_space.m_vect= m_proj_space.m_vect * m_ritz_pairs.U.leftCols(size); // ? 
+        m_proj_space.vect_m_vect = m_proj_space.vect.transpose() * m_proj_space.m_vect;
+    }
 
-    // struct ProjectedSpace {
-    //     Eigen::MatrixXd V;   // basis of vectors
-    //     Eigen::MatrixXd AV;  // A * V
-    //     Eigen::MatrixXd T;   // V.T * A * V
-    //     Index search_space() const {
-    //     return V.cols();
-    //     };                  // size of the projection i.e. number of cols in V
-    //     Index size_update;  // size update ...
-    //     std::vector<bool> root_converged;  // keep track of which root have onverged
-    // };
+    void full_update_projected_space() 
+    {
+        m_proj_space.m_vect = m_op * m_proj_space.vect;
+        m_nmatop ++;
 
-// public
-    // Index compute(SortRule selection = SortRule::LargestMagn, Index maxit = 1000,
-    //               Scalar tol = 1e-10) 
-    // {
+        m_proj_space.vect_m_vect = m_proj_space.vect.transpose() * m_proj_space.m_vect;        
         
+    }
+
+    void update_projected_space()
+    {
+      Index old_dim = m_proj_space.vect_m_vect.cols();
+      Index new_dim = m_proj_space.vect.cols();
+      Index nvec = new_dim - old_dim;
+
+      m_proj_space.m_vect.conservativeResize(Eigen::NoChange, new_dim);
+      
+      m_proj_space.m_vect.rightCols(nvec) = m_op * m_proj_space.vect.rightCols(nvec);
+      m_nmatop ++;
+
+      Matrix tmp_proj = m_proj_space.vect.transpose() * m_proj_space.m_vect.rightCols(nvec);
+      m_proj_space.vect_m_vect.conservativeResize(new_dim, new_dim);
+      m_proj_space.vect_m_vect.rightCols(nvec) = tmp_proj;
+      m_proj_space.vect_m_vect.bottomLeftCorner(nvec, old_dim) =
+        m_proj_space.vect_m_vect.topRightCorner(old_dim, nvec).transpose();
+    }
+
+public:
+    Index compute(SortRule selection = SortRule::LargestMagn, Index maxit = 1000,
+                  Scalar tol = 1e-10)
+    {
+        
+        for(m_niter=0; m_niter < maxit; m_niter++)
+        {
+            bool do_restart = (m_proj_space.current_size() > m_max_search_space_size);
+            
+            if (do_restart) {
+                restart(m_initial_search_space_size);
+            }
+
+            update_projected_space();
+            
 
 
-    // }
-
-    // virtual void get_correction_vector = 0;
+        }
+    }
 };
 
-}  // namespace spectra
-#endif // SPECTRA_JD_SYM_EIGS_BASE_H
+}  // namespace Spectra
+#endif  // SPECTRA_JD_SYM_EIGS_BASE_H
--- test/JDSymEigsBase.cpp
+++ test/JDSymEigsBase.cpp
@@ -16,10 +16,31 @@ using Vector = Eigen::VectorXd;
 using ComplexMatrix = Eigen::MatrixXcd;
 using ComplexVector = Eigen::VectorXcd;
 using SpMatrix = Eigen::SparseMatrix<double>;
+using Index =Eigen::Index;
+template <typename Scalar,
+          typename OpType>
+class JDMock : public JDSymEigsBase<Scalar,OpType>{
+
+public:
+JDMock(OpType& op, Index nev):JDSymEigsBase<Scalar,OpType>(op,nev){}
+  Matrix SetupInitialSearchSpace()const{
+      return Matrix::Zero(0,0);
+  }
+
+  Matrix CalculateCorrectionVector() const{
+       return Matrix::Zero(0,0);
+  }
+
+
+};
 
 TEST_CASE("Constructing JDSymObject", "[eigs_gen]")
 {
     const Matrix A = Eigen::MatrixXd::Random(10, 10);
     DenseGenMatProd<double> op(A);
-    JDSymEigsBase<double, DenseGenMatProd<double> > eigs(op, 5);
+    JDMock<double, DenseGenMatProd<double> > eigs(op, 5);
+
+    REQUIRE( eigs.num_iterations() == 0 );
+    REQUIRE( eigs.num_operations() == 0 );
+    REQUIRE( eigs.info() == Spectra::CompInfo::NotComputed );
 }
