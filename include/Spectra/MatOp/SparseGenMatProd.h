fed0467d1f55e6d5cbaf7cabbc809709a2759131
Victor Azizi <victor@lipsum.eu>
2020-06-30 11:44:18 +0200

    bugfix

--- include/Spectra/MatOp/SparseGenMatProd.h
+++ include/Spectra/MatOp/SparseGenMatProd.h
@@ -1,4 +1,4 @@
-u  // Copyright (C) 2016-2020 Yixuan Qiu <yixuan.qiu@cos.name>
+// Copyright (C) 2016-2020 Yixuan Qiu <yixuan.qiu@cos.name>
 //
 // This Source Code Form is subject to the terms of the Mozilla
 // Public License v. 2.0. If a copy of the MPL was not distributed
