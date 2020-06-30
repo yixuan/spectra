5c9d8b43f533c086c37761e615de070c1a039134
felipez <tifonzafel@gmail.com>
2020-06-30 16:30:58 +0200

    implement SetupInitialSearchSpace CalculateCorrectionVector #7

--- include/Spectra/JDSymEigsDPR.h
+++ include/Spectra/JDSymEigsDPR.h
@@ -9,33 +9,59 @@
 
 #include <Eigen/Dense>
 #include "JDSymEigsBase.h"
+#include "Util/SelectionRule.h"
 
 namespace Spectra {
 
 template <typename Scalar, typename OpType>
 class JDSymEigsDPR : public JDSymEigsBase<Scalar, OpType>
 {
-public:
-    Matrix SetupInitialSearchSpace() const final
+private:
+    Vector diagonal_(operator_dimension_);
+    std::vector<Eigen::Index> indices_sorted_;
+
+    void extract_diagonal()
     {
+        for (Index i = 0; i < operator_dimension_; i++)
+        {
+            diagonal_(i) = matrix_operator_(i, i);
+        }
+    }
+
+    void calculate_indices_diagonal_sorted(SortRule selection)
+    {
+        indices_sorted_ = argsort(selection, diagonal_);
+    }
+
+public:
+    /// Create initial search space based on the diagonal
+    // and the spectrum'target (highest or lowest)
+    Matrix SetupInitialSearchSpace(SortRule selection) const final
+    {
+        extract_diagonal();
+        calculate_indices_diagonal_sorted(selection);
+
+        Matrix initial_basis = Matrix::Zero(operator_dimension_, initial_search_space_size_);
+
+        for (Index k = 0; k < initial_search_space_size_; k++)
+        {
+            Index row = indices_sorted_[k];
+            initial_basis.coeff(row, k) = diagonal_[k];
+        }
+        return initial_basis;
     }
 
     /// compute the corrections using the DPR method.
     /// \return new correction vectors.
     Matrix CalculateCorrectionVector() const final
     {
-        Vector diagonal(operator_dimension_);
-        for (Index i = 0; i < operator_dimension_; i++)
-        {
-            diagonal(i) = matrix_operator_(i, i);
-        }
         Index nresidues = search_space_.size();
+        const Matrix& residues = Residues();
         const Vector& eigenvalues = ritz_pairs_.RitzValues();
         Matrix correction = Matrix::zero(operator_dimension_, nresidues);
         for (Index k = 0; k < nresidues; k++)
         {
-            Vector tmp = Vector::Constant(eigenvalues(k)) - diagonal;
-            correction.col(k) = residues / tmp;
+            correction.col(k) = residues.col(k) / (Vector::Constant(eigenvalues(k)) - diagonal_);
         }
         return correction;
     }
