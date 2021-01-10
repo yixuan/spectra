a3b32d8418a9d242886f44d4d5f7a85f49981424
Yixuan Qiu <yixuanq@gmail.com>
2021-01-10 12:02:35 +0800

    update benchmarking code

--- benchmark/Cpp.cpp
+++ benchmark/Cpp.cpp
@@ -19,7 +19,7 @@ void eigs_sym_Cpp(MatrixXd &M, VectorXd &init_resid, int k, int m,
     start = get_wall_time();
 
     DenseSymMatProd<double> op(M);
-    SymEigsSolver<double, DenseSymMatProd<double>> eigs(op, k, m);
+    SymEigsSolver<DenseSymMatProd<double>> eigs(op, k, m);
     eigs.init(init_resid.data());
 
     int nconv = eigs.compute(SortRule::LargestMagn);
@@ -49,7 +49,7 @@ void eigs_gen_Cpp(MatrixXd &M, VectorXd &init_resid, int k, int m,
     start = get_wall_time();
 
     DenseGenMatProd<double> op(M);
-    GenEigsSolver<double, DenseGenMatProd<double>> eigs(op, k, m);
+    GenEigsSolver<DenseGenMatProd<double>> eigs(op, k, m);
     eigs.init(init_resid.data());
 
     int nconv = eigs.compute(SortRule::LargestMagn);
