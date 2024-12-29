45ed53a6ed7ac5c5dfbbc4cfd0a9be6597d6ccfe
Yixuan Qiu <yixuanq@gmail.com>
2024-12-29 21:22:43 +0800

    testing code for complex Arnoldi factorization

--- test/Arnoldi.cpp
+++ test/Arnoldi.cpp
@@ -13,13 +13,15 @@ using Matrix = Eigen::MatrixXd;
 using Vector = Eigen::VectorXd;
 using Index = Eigen::Index;
 
-template <typename FacType>
-void run_test(FacType &fac, const Matrix& A, int m)
+template <typename FacType, typename MatType>
+void run_test(FacType &fac, const MatType& A, int m)
 {
+    using VecType = Eigen::Matrix<typename MatType::Scalar, Eigen::Dynamic, 1>;
+
     // Initialization
     const int n = A.rows();
-    Vector v0 = Vector::Random(n);
-    Eigen::Map<const Vector> v0map(v0.data(), n);
+    VecType v0 = VecType::Random(n);
+    Eigen::Map<const VecType> v0map(v0.data(), n);
     Index op_counter = 0;
     fac.init(v0map, op_counter);
 
@@ -30,14 +32,14 @@ void run_test(FacType &fac, const Matrix& A, int m)
 
     // A*V = V*H + f*e
     constexpr double tol = 1e-12;
-    Matrix V = fac.matrix_V().leftCols(k1);
-    Matrix H = fac.matrix_H().topLeftCorner(k1, k1);
-    Matrix resid = A * V - V * H;
-    Vector f = fac.vector_f();
+    MatType V = fac.matrix_V().leftCols(k1);
+    MatType H = fac.matrix_H().topLeftCorner(k1, k1);
+    MatType resid = A * V - V * H;
+    VecType f = fac.vector_f();
     INFO("AV - VH = \n" << resid);
     INFO("f = " << f.transpose());
     // Test residual
-    REQUIRE((resid.rightCols<1>() - f).cwiseAbs().maxCoeff() == Approx(0.0).margin(tol));
+    REQUIRE((resid.template rightCols<1>() - f).cwiseAbs().maxCoeff() == Approx(0.0).margin(tol));
 
     // Factorization to m/2
     fac.factorize_from(1, m / 2, op_counter);
@@ -53,7 +55,7 @@ void run_test(FacType &fac, const Matrix& A, int m)
     INFO("f = " << f.transpose());
     // Test residual
     REQUIRE(resid.leftCols(k2 - 1).cwiseAbs().maxCoeff() == Approx(0.0).margin(tol));
-    REQUIRE((resid.rightCols<1>() - f).cwiseAbs().maxCoeff() == Approx(0.0).margin(tol));
+    REQUIRE((resid.template rightCols<1>() - f).cwiseAbs().maxCoeff() == Approx(0.0).margin(tol));
 
     // Factorization to m
     fac.factorize_from(m / 2, m, op_counter);
@@ -65,18 +67,18 @@ void run_test(FacType &fac, const Matrix& A, int m)
     H = fac.matrix_H().topLeftCorner(k3, k3);
     resid = A * V - V * H;
     f = fac.vector_f();
-    Matrix VtV = V.transpose() * V;
+    MatType VHV = V.adjoint() * V;
     INFO("V = \n" << V);
     INFO("H = \n" << H);
-    INFO("V'V = \n" << VtV);
+    INFO("V^H * V = \n" << VHV);
     INFO("AV - VH = \n" << resid);
     INFO("f = " << f.transpose());
     // Test orthogonality of V
-    Matrix iden = Matrix::Identity(m, m);
-    REQUIRE((VtV - iden).cwiseAbs().maxCoeff() == Approx(0.0).margin(tol));
+    MatType iden = MatType::Identity(m, m);
+    REQUIRE((VHV - iden).cwiseAbs().maxCoeff() == Approx(0.0).margin(tol));
     // Test residual
     REQUIRE(resid.leftCols(k3 - 1).cwiseAbs().maxCoeff() == Approx(0.0).margin(tol));
-    REQUIRE((resid.rightCols<1>() - f).cwiseAbs().maxCoeff() == Approx(0.0).margin(tol));
+    REQUIRE((resid.template rightCols<1>() - f).cwiseAbs().maxCoeff() == Approx(0.0).margin(tol));
 }
 
 TEST_CASE("Lanczos factorization of symmetric real matrix", "[Lanczos]")
@@ -110,6 +112,24 @@ TEST_CASE("Arnoldi factorization of general real matrix", "[Arnoldi]")
     AOpType Aop(A);
     ArnoldiOpType arn(Aop, IdentityBOp());
     Arnoldi<double, ArnoldiOpType> fac(arn, m);
+
+    run_test(fac, A, m);
+}
+
+TEST_CASE("Arnoldi factorization of general complex matrix", "[Arnoldi]")
+{
+    std::srand(123);
+    const int n = 10;
+    Eigen::MatrixXcd A = Eigen::MatrixXcd::Random(n, n);
+    const int m = 6;
+
+    using Scalar = std::complex<double>;
+    using AOpType = DenseGenMatProd<Scalar>;
+    using ArnoldiOpType = ArnoldiOp<Scalar, AOpType, IdentityBOp>;
+
+    AOpType Aop(A);
+    ArnoldiOpType arn(Aop, IdentityBOp());
+    Arnoldi<Scalar, ArnoldiOpType> fac(arn, m);
 
     run_test(fac, A, m);
 }
