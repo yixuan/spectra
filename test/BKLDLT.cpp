149bfdecb115fe4397a1fab35dd2764de4d0c67b
Yixuan Qiu <yixuanq@gmail.com>
2025-01-01 16:37:26 +0800

    testing code for Hermitian BKLDLT

--- test/BKLDLT.cpp
+++ test/BKLDLT.cpp
@@ -8,26 +8,31 @@ using namespace Spectra;
 
 using Matrix = Eigen::MatrixXd;
 using Vector = Eigen::VectorXd;
+using ComplexMatrix = Eigen::MatrixXcd;
+using ComplexVector = Eigen::VectorXcd;
 
 // Solve (A - s * I)x = b
-void run_test(const Matrix& A, const Vector& b, double s)
+template <typename MatType, typename VecType>
+void run_test(const MatType& A, const VecType& b, double s)
 {
+    using Scalar = typename MatType::Scalar;
+
     // Test decomposition using only the lower triangular part
-    BKLDLT<double> decompL(A, Eigen::Lower, s);
+    BKLDLT<Scalar> decompL(A, Eigen::Lower, s);
     REQUIRE(decompL.info() == CompInfo::Successful);
 
     // Test decomposition using only the upper triangular part
-    BKLDLT<double> decompU(A, Eigen::Upper, s);
+    BKLDLT<Scalar> decompU(A, Eigen::Upper, s);
     REQUIRE(decompU.info() == CompInfo::Successful);
 
     // Test whether the solutions are identical
-    Vector solL = decompL.solve(b);
-    Vector solU = decompU.solve(b);
+    VecType solL = decompL.solve(b);
+    VecType solU = decompU.solve(b);
     REQUIRE((solL - solU).cwiseAbs().maxCoeff() == 0.0);
 
     // Test the accuracy of the solution
     constexpr double tol = 1e-9;
-    Vector resid = A * solL - s * solL - b;
+    VecType resid = A * solL - s * solL - b;
     INFO("||(A - s * I)x - b||_inf = " << resid.cwiseAbs().maxCoeff());
     REQUIRE(resid.cwiseAbs().maxCoeff() == Approx(0.0).margin(tol));
 }
@@ -63,6 +68,42 @@ TEST_CASE("BKLDLT decomposition of symmetric real matrix [1000x1000]", "[BKLDLT]
     Matrix A = Matrix::Random(n, n);
     A = (A + A.transpose()).eval();
     Vector b = Vector::Random(n);
+    const double shift = 1.0;
+
+    run_test(A, b, shift);
+}
+
+TEST_CASE("BKLDLT decomposition of Hermitian matrix [10x10]", "[BKLDLT]")
+{
+    std::srand(123);
+    const int n = 10;
+    ComplexMatrix A = ComplexMatrix::Random(n, n);
+    A = (A + A.adjoint()).eval();
+    ComplexVector b = ComplexVector::Random(n);
+    const double shift = 1.0;
+
+    run_test(A, b, shift);
+}
+
+TEST_CASE("BKLDLT decomposition of Hermitian matrix [100x100]", "[BKLDLT]")
+{
+    std::srand(123);
+    const int n = 100;
+    ComplexMatrix A = ComplexMatrix::Random(n, n);
+    A = (A + A.adjoint()).eval();
+    ComplexVector b = ComplexVector::Random(n);
+    const double shift = 1.0;
+
+    run_test(A, b, shift);
+}
+
+TEST_CASE("BKLDLT decomposition of Hermitian matrix [1000x1000]", "[BKLDLT]")
+{
+    std::srand(123);
+    const int n = 1000;
+    ComplexMatrix A = ComplexMatrix::Random(n, n);
+    A = (A + A.adjoint()).eval();
+    ComplexVector b = ComplexVector::Random(n);
     const double shift = 1.0;
 
     run_test(A, b, shift);
