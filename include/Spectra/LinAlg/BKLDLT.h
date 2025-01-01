cefc1c25973be65cc998e659a523da594d262d0e
Yixuan Qiu <yixuanq@gmail.com>
2025-01-01 16:43:13 +0800

    update copyright year

--- include/Spectra/LinAlg/BKLDLT.h
+++ include/Spectra/LinAlg/BKLDLT.h
@@ -1,4 +1,4 @@
-// Copyright (C) 2019-2024 Yixuan Qiu <yixuan.qiu@cos.name>
+// Copyright (C) 2019-2025 Yixuan Qiu <yixuan.qiu@cos.name>
 //
 // This Source Code Form is subject to the terms of the Mozilla
 // Public License v. 2.0. If a copy of the MPL was not distributed
